11d55808f5da24501546624e54e3ceaa01c9633c
Clayton-W-J <106715412+Clayton-W-J@users.noreply.github.com>
2024-05-27 22:39:59 -0500

    Update piperinstall.sh

--- Ubuntu Desktop/piperinstall.sh
+++ Ubuntu Desktop/piperinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Piper is a GTK+ application to configure gaming mice and keyboard. This bash script is designed to install Piper. More info can be found here: https://github.com/libratbag/piper
-sudo add-apt-repository universe
+sudo add-apt-repository universe -y
 sudo apt update
-sudo apt install piper
+sudo apt install piper -y
 sudo systemctl enable ratbagd.service
 sudo systemctl daemon-reload
 sudo systemctl reload dbus.service

2eb4731824179d5195640e4657afda1c04cc0f0e
Clayton-W-J <106715412+Clayton-W-J@users.noreply.github.com>
2024-06-01 23:55:18 -0500

    Create grafana_install.sh

---
+++ analytics_frontends/grafana_install.sh
@@ -0,0 +1,11 @@
+#Install the signing key
+wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
+
+#Add repository to grafana.list
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" > /etc/apt/sources.list.d/grafana.list
+
+#apt update and install (or update)
+apt update && apt install grafana -y
+
+#Start the server
+systemctl enable --now grafana-server

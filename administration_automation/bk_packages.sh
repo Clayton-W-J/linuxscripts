84ca4e420b67b6404f962285e0536de288f4efbb
Clayton-W-J <106715412+Clayton-W-J@users.noreply.github.com>
2024-06-11 21:21:52 -0500

    Update bk_packages.sh

--- administration_automation/bk_packages.sh
+++ administration_automation/bk_packages.sh
@@ -1,4 +1,4 @@
-# This script is intended to be used to back all apt packages on a linux machines using apt
+# This script is intended to be used to back all apt packages on a linux machine using apt and send it to a network location.
 
 #!/bin/bash
 

81f09a897f6f59f2335aad9efb29fc75c4cfb5fa
CJ <106715412+Clayton-W-J@users.noreply.github.com>
2025-07-04 03:39:18 -0500

    Update Install-PVE-QEMU-Guest-Agent.sh

--- administration_automation/Install-PVE-QEMU-Guest-Agent.sh
+++ administration_automation/Install-PVE-QEMU-Guest-Agent.sh
@@ -4,11 +4,9 @@
 if [ ! -f /usr/sbin/qemu-ga ]; then
     read -p "Would you like to install the Proxmox QEMU Guest Agent? (y/n): " INSTALL_QEMU
     if [[ "$INSTALL_QEMU" =~ ^[Yy]$ ]]; then
-        {
             echo "Installing, enabling, and starting the QEMU Guest Agent..."
             sudo apt install qemu-guest-agent -y
             sudo systemctl enable --now qemu-guest-agent
-        } >> "$LOG_FILE" 2>&1
         echo "QEMU Guest Agent installed and enabled!"
     else
         echo "Skipping QEMU Guest Agent installation."

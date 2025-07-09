696ca1d9b3fd8f5f42df5f9ddcce28daa47aa305
CJ <106715412+Clayton-W-J@users.noreply.github.com>
2025-07-08 23:04:44 -0500

    Update post-clone.sh

--- proxmox/post-clone.sh
+++ proxmox/post-clone.sh
@@ -74,8 +74,8 @@ sudo netplan apply
 # -------------------------------
 
 # Add Tailscale's package signing key and repository:
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/>
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/>
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
 # Install Tailscale
 sudo apt-get update -y
